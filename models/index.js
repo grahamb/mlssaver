486a9ff790b850b9164a79499e63e678961aa326
Graham Ballantyne <grahamb@sfu.ca>
2014-04-10 07:50:21 -0700

    fix botched env var

--- models/index.js
+++ models/index.js
@@ -4,8 +4,8 @@ var schema = mongoose.Schema;
 var env = process.env.NODE_ENV || 'test';
 
 if (env === 'production') {
-    config.production.username = process[env].mongouser;
-    config.production.pass = process[env].mongopass;
+    config.production.username = process.env.mongouser;
+    config.production.pass = process.env.mongopass;
 }
 // var mongourl = 'mongodb://' + mongouser + ':' + mongopass + '@oceanic.mongohq.com:10018/mls'
 mongoose.connect(config[env].hostname, config[env].port, config[env]);
