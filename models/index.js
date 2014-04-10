fc48652a98cccbea1b4348dc79430cc8408b009c
Graham Ballantyne <grahamb@sfu.ca>
2014-04-10 10:59:10 -0700

    back out mongodb config change

--- models/index.js
+++ models/index.js
@@ -1,15 +1,9 @@
 var mongoose = require('mongoose');
-var config = require('../config.json');
 var schema = mongoose.Schema;
 var env = process.env.NODE_ENV || 'test';
 
-if (env === 'production') {
-    config.production.username = process.env.mongouser;
-    config.production.password = process.env.mongopass;
-}
-
-console.log(config[env]);
-mongoose.connect(config[env].hostname, config[env].database, config[env].port, config[env]);
+var mongourl = process.env.NODE_ENV === 'production' ? process.env.mongourl : 'mongodb://localhost/mls';
+mongoose.connect(mongourl);
 
 module.exports.Listing = require('./listing');
 module.exports.mongoose = mongoose;
