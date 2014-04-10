591c60925f17c28cf5504c447db4232fa717ec74
Graham Ballantyne <grahamb@sfu.ca>
2014-04-09 22:14:30 -0700

    use config for mongo settings
    Signed-off-by: Graham Ballantyne <grahamb@sfu.ca>

--- models/index.js
+++ models/index.js
@@ -1,10 +1,14 @@
 var mongoose = require('mongoose');
+var config = require('../config.json');
 var schema = mongoose.Schema;
+var env = process.env.NODE_ENV || 'test';
 
-var mongouser = process.env.mongouser;
-var mongopass = process.env.mongopass;
-var mongourl = 'mongodb://' + mongouser + ':' + mongopass + '@oceanic.mongohq.com:10018/mls'
-mongoose.connect(mongourl);
+if (env === 'production') {
+    config.production.username = process[env].mongouser;
+    config.production.pass = process[env].mongopass;
+}
+// var mongourl = 'mongodb://' + mongouser + ':' + mongopass + '@oceanic.mongohq.com:10018/mls'
+mongoose.connect(config[env].hostname, config[env].port, config[env]);
 
 module.exports.Listing = require('./listing');
 module.exports.mongoose = mongoose;
